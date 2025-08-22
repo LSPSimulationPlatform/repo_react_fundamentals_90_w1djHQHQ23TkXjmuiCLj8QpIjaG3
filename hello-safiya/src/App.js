bdfddbcfec7aacc8f7d191d9318b41befd687251
Safiye-Rustamli <rustamlisafiya2012@gmail.com>
2025-08-22 11:43:22 +0000

    My first project commited

---
+++ hello-safiya/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+   <h1>Hello Safiya</h1>
+   <label style={{fontSize:"30px",color:"blue"}}>Its my first project</label>
+   </div>
+  
+  )
+}
+
+export default App;
